891f3a529ab355801d0347a13f550b042eae99c3
Pratyay360 <pratyaymustafi@outlook.com>
2026-06-17 08:06:08 +0000

    build

--- module.go
+++ module.go
@@ -15,7 +15,7 @@ import (
 	"github.com/moby/moby/client"
 	"go.uber.org/zap"
 	"reflect"
-	)
+)
 
 func init() {
 	caddy.RegisterModule(new(ContainerList))
@@ -66,10 +66,7 @@ func (cl *ContainerList) getClient(ctx context.Context) (*client.Client, error) 
 			continue
 		}
 
-		c, err := client.NewClientWithOpts(
-			client.WithHost("unix://"+sock),
-			client.WithAPIVersionNegotiation(),
-		)
+		c, err := client.New(client.WithHost("unix://"+sock), client.WithAPIVersionNegotiation())
 		if err != nil {
 			continue
 		}
@@ -132,69 +129,68 @@ func (cl *ContainerList) ServeHTTP(w http.ResponseWriter, r *http.Request, next 
 		return caddyhttp.Error(http.StatusInternalServerError, err)
 	}
 
-		// Normalize result to a slice of container reflect.Values regardless of return type
-		var containerVals []reflect.Value
-		rv := reflect.ValueOf(containersRes)
-		if rv.Kind() == reflect.Ptr {
-			rv = rv.Elem()
-		}
-		if rv.IsValid() {
-			if rv.Kind() == reflect.Slice {
-				for i := 0; i < rv.Len(); i++ {
-					containerVals = append(containerVals, rv.Index(i))
+	// Normalize result to a slice of container reflect.Values regardless of return type
+	var containerVals []reflect.Value
+	rv := reflect.ValueOf(containersRes)
+	if rv.Kind() == reflect.Pointer {
+		rv = rv.Elem()
+	}
+	if rv.IsValid() {
+		if rv.Kind() == reflect.Slice {
+			for i := 0; i < rv.Len(); i++ {
+				containerVals = append(containerVals, rv.Index(i))
+			}
+		} else if rv.Kind() == reflect.Struct {
+			// Find first slice field and use it
+			for _, f := range rv.Fields() {
+				if f.Kind() == reflect.Slice {
+					for j := 0; j < f.Len(); j++ {
+						containerVals = append(containerVals, f.Index(j))
+					}
+					break
 				}
-			} else if rv.Kind() == reflect.Struct {
-				// Find first slice field and use it
-				for i := 0; i < rv.NumField(); i++ {
-					f := rv.Field(i)
-					if f.Kind() == reflect.Slice {
-						for j := 0; j < f.Len(); j++ {
-							containerVals = append(containerVals, f.Index(j))
-						}
-						break
+			}
+		}
+	}
+
+	if len(containerVals) == 0 {
+		return caddyhttp.Error(http.StatusInternalServerError, fmt.Errorf("unexpected container list result type: %T", containersRes))
+	}
+
+	result := make([]ContainerInfo, 0, len(containerVals))
+	for _, cv := range containerVals {
+		// Extract name
+		name := ""
+		if fv := cv.FieldByName("Names"); fv.IsValid() && fv.Kind() == reflect.Slice && fv.Type().Elem().Kind() == reflect.String {
+			if fv.Len() > 0 {
+				name = strings.TrimPrefix(fv.Index(0).String(), "/")
+			}
+		} else if nv := cv.FieldByName("Name"); nv.IsValid() && nv.Kind() == reflect.String {
+			name = strings.TrimPrefix(nv.String(), "/")
+		}
+
+		// Extract ports
+		var ports []string
+		seen := make(map[uint64]bool)
+		if pv := cv.FieldByName("Ports"); pv.IsValid() && pv.Kind() == reflect.Slice {
+			for i := 0; i < pv.Len(); i++ {
+				portVal := pv.Index(i)
+				// try to find field PrivatePort
+				if pp := portVal.FieldByName("PrivatePort"); pp.IsValid() && (pp.Kind() >= reflect.Int && pp.Kind() <= reflect.Int64 || pp.Kind() >= reflect.Uint && pp.Kind() <= reflect.Uint64) {
+					portNum := pp.Convert(reflect.TypeFor[uint64]()).Interface().(uint64)
+					if portNum > 0 && !seen[portNum] {
+						seen[portNum] = true
+						ports = append(ports, fmt.Sprintf("%d", portNum))
 					}
 				}
 			}
 		}
-
-		if len(containerVals) == 0 {
-			return caddyhttp.Error(http.StatusInternalServerError, fmt.Errorf("unexpected container list result type: %T", containersRes))
-		}
-
-		result := make([]ContainerInfo, 0, len(containerVals))
-		for _, cv := range containerVals {
-			// Extract name
-			name := ""
-			if fv := cv.FieldByName("Names"); fv.IsValid() && fv.Kind() == reflect.Slice && fv.Type().Elem().Kind() == reflect.String {
-				if fv.Len() > 0 {
-					name = strings.TrimPrefix(fv.Index(0).String(), "/")
-				}
-			} else if nv := cv.FieldByName("Name"); nv.IsValid() && nv.Kind() == reflect.String {
-				name = strings.TrimPrefix(nv.String(), "/")
-			}
-
-			// Extract ports
-			var ports []string
-			seen := make(map[uint64]bool)
-			if pv := cv.FieldByName("Ports"); pv.IsValid() && pv.Kind() == reflect.Slice {
-				for i := 0; i < pv.Len(); i++ {
-					portVal := pv.Index(i)
-					// try to find field PrivatePort
-					if pp := portVal.FieldByName("PrivatePort"); pp.IsValid() && (pp.Kind() >= reflect.Int && pp.Kind() <= reflect.Int64 || pp.Kind() >= reflect.Uint && pp.Kind() <= reflect.Uint64) {
-						portNum := pp.Convert(reflect.TypeOf(uint64(0))).Interface().(uint64)
-						if portNum > 0 && !seen[portNum] {
-							seen[portNum] = true
-							ports = append(ports, fmt.Sprintf("%d", portNum))
-						}
-					}
-				}
-			}
 
 		result = append(result, ContainerInfo{
-				Name:  name,
-				Ports: strings.Join(ports, ", "),
-			})
-		}
+			Name:  name,
+			Ports: strings.Join(ports, ", "),
+		})
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(result)
